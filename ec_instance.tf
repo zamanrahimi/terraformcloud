b3b73058dd92d03e9097c8d782b724cac18df34e
User1 <user1@User1s-MacBook-Pro.local>
2023-05-24 14:14:34 -0500

    test1

--- ec_instance.tf
+++ ec_instance.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-west-2"
-  access_key = ""
-  secret_key = ""
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 data "aws_ami" "ubuntu" {
