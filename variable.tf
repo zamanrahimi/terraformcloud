a22e1fddf44774e32ee2312a4f463c08ad931acc
User1 <user1@User1s-MacBook-Pro.local>
2023-05-24 14:37:22 -0500

    test2

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type        = string
+
+  description = "secret key"
+}
+
+variable "AAWS_SECRET_ACCESS_KEY" {
+  type        = string
+  description = "access key"
+}
